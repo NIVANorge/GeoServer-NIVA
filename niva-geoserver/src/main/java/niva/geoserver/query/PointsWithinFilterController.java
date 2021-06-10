430d3feefb7f0a55b773c404b5ca9747c4969189
Roar Brænden <roar.branden@niva.no>
2021-06-10 14:25:08 +0200

    Projection is done within getFeatures
    ... original CRS is specified in userData.

--- niva-geoserver/src/main/java/niva/geoserver/query/PointsWithinFilterController.java
+++ niva-geoserver/src/main/java/niva/geoserver/query/PointsWithinFilterController.java
@@ -3,28 +3,18 @@ package niva.geoserver.query;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.logging.Logger;
-
 import org.geoserver.catalog.Catalog;
 import org.geoserver.rest.RestException;
-
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.text.cql2.CQL;
 import org.geotools.filter.text.cql2.CQLException;
-import org.geotools.geometry.jts.JTS;
-import org.geotools.referencing.CRS;
 import org.geotools.util.logging.Logging;
-
-
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
-import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.referencing.operation.TransformException;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,18 +23,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
 import org.locationtech.jts.geom.Geometry;
-
-
 
 /**
  * Returns a collection of points that is within a given polygon.
- * Spesifikasjonen er som følger:
+ * Specification is as follows:
  * 
  * /query/{workspace}/{layer}/filter/{workspace2}/{layer2}/{filter}/features.json
  * 
- * Resultatet returneres som et array med de resulterende features.
+ * Response is a json {features:[{...},{...}....]}
  * @author Roar Brænden
  *
  */
@@ -85,27 +72,19 @@ public class PointsWithinFilterController extends QueryBaseController {
 
 	            while (iter.hasNext()) {
 	                final SimpleFeature feat = iter.next();
-	                LOGGER.fine("Record:" + feat.toString());
+	                LOGGER.fine("Record:" + feat.getID());
 	                final Geometry geom = (Geometry)feat.getDefaultGeometry();
 	                
 	                uni = (uni != null && geom != null ? uni.union(geom) : geom);
 	            }
 			}
 			
-			final CoordinateReferenceSystem pointCrs = pointSource.getSchema().getCoordinateReferenceSystem();
-			final CoordinateReferenceSystem polyCrs = polySource.getSchema().getCoordinateReferenceSystem();
-			
-			if (!CRS.equalsIgnoreMetadata(pointCrs,  polyCrs)) {
-				LOGGER.fine("Transforming from:"+ polyCrs.toWKT() + " to:"+ pointCrs.toWKT());
-				uni = JTS.transform(uni, CRS.findMathTransform(polyCrs, pointCrs, true));
-			}
-
 			final Filter within = ff.within(ff.property(pointSource.getSchema().getGeometryDescriptor().getLocalName()),
 			                                ff.literal(uni));
 
 			return createResultMap(pointSource.getFeatures(within));
 		}
-		catch (TransformException | FactoryException | CQLException | IOException ex) {
+		catch (CQLException | IOException ex) {
 			throw new RestException(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
