c2cf62e83d9f6391ea5f89d573c075caaf477f91
Roar Brænden <roar.branden@niva.no>
2025-11-10 20:17:04 +0100

    Working test

--- niva-geoserver/src/test/java/niva/geoserver/wfs/KorallrevWFSTest.java
+++ niva-geoserver/src/test/java/niva/geoserver/wfs/KorallrevWFSTest.java
@@ -1,6 +1,7 @@
 package niva.geoserver.wfs;
 
 import java.util.Map;
+import java.util.logging.Level;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Serializable;
@@ -9,17 +10,26 @@ import org.geoserver.catalog.FeatureTypeInfo;
 import org.geoserver.catalog.LayerInfo;
 import org.geoserver.catalog.StoreInfo;
 import org.geoserver.catalog.impl.DataStoreInfoImpl;
+import org.geotools.util.logging.Logging;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import niva.geoserver.data.NivaTestSupport;
 
+/**
+ * Tests for Korallrev WFS layer cascading.
+ * HIs WFS service hosted at geonorge.no
+ */
 public class KorallrevWFSTest extends NivaTestSupport {
 	
 	private static final String WFS_URL = "https://wfs.geonorge.no/skwms1/wfs.korallrev?";
 	@Before
 	public void setupKorallrevWFSProxy() throws Exception {
+		System.setProperty("org.geotools.http.logging", "TRUE");
+		Logging.getLogger("org.geotools.http").setLevel(Level.FINE);
+		Logging.getLogger("org.geotools.data.wfs").setLevel(Level.FINE);
 		Catalog catalog = getCatalog();
 		
 		StoreInfo store = new DataStoreInfoImpl(catalog);
@@ -57,16 +67,19 @@ public class KorallrevWFSTest extends NivaTestSupport {
 		catalog.remove(store);
 	}
 	
+	/**
+	 * Test a GetFeature request similar to the one QGIS would send. Should return one random feature.
+	 */
 	@Test
 	public void testQGISGetFeatureCall() throws Exception {
 		String path = "wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=no.norgedigitalt:korallrev&STARTINDEX=0&COUNT=1&SRSNAME=urn:ogc:def:crs:EPSG::4258&BBOX=-90,-180,90,180,urn:ogc:def:crs:EPSG::4258";
-		//String path = "wfs?SERVICE=WFS&REQUEST=GetCapabilities&VERSION=2.0.0";
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(get(path)))) {
 			String line;
+			StringBuilder response = new StringBuilder();
 			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
+				response.append(line);
 			}
+			Assert.assertTrue(response.toString().contains("<wfs:member><no.norgedigitalt:korallrev"));
 		}
 	}
-
 }
